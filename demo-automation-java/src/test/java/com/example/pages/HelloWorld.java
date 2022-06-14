a7969e227e037d5039453c84ab0e00c88e772beb
DuVH <haodu1498@gmail.com>
2022-06-14 21:21:30 +0700

    Authentication check

--- demo-automation-java/src/test/java/com/example/pages/HelloWorld.java
+++ demo-automation-java/src/test/java/com/example/pages/HelloWorld.java
@@ -3,6 +3,7 @@ package com.example.pages;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Authentication check.");
     }
     
 }
