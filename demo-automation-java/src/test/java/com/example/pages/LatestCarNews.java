416b0a7e2ab868d1b6b3740b63c8ad72a5473116
DoThiToi <dothitoi01032000@gmail.com>
2022-06-14 19:24:37 +0700

    CarNew

--- demo-automation-java/src/test/java/com/example/pages/LatestCarNews.java
+++ demo-automation-java/src/test/java/com/example/pages/LatestCarNews.java
@@ -5,8 +5,14 @@ import org.openqa.selenium.WebElement;
 
 public class LatestCarNews {
     private WebElement keyword;
+    WebElement txUsername = keyword.findElement(By.xpath("//input[@id='user-name']"));
+    WebElement txPassword = keyword.findElement(By.xpath("//input[@id='password']"));
+    WebElement btLogin = keyword.findElement(By.xpath("//input[@id='login-button']")); 
     WebElement txtUsername = keyword.findElement(By.xpath("//input[@id='user-name']"));
     WebElement txtPassword = keyword.findElement(By.xpath("//input[@id='password']"));
     WebElement btnLogin = keyword.findElement(By.xpath("//input[@id='login-button']")); 
+    
+    //dhfjsdfsdnf
+    //dcnfbscsbf
 }
 
