c369bf052953993ab7e70019389859a7e195502a
TuND20 <tund.fit@gmail.com>
2022-06-16 09:12:29 +0700

    create extent report apply for project: - create new class ExtentManager - create new class ExtentTestManager - create new class ReportListener

--- demo-automation-java/src/test/java/com/example/tests/TestMainWorkFlow1.java
+++ demo-automation-java/src/test/java/com/example/tests/TestMainWorkFlow1.java
@@ -1,9 +1,12 @@
 package com.example.tests;
 
+import com.example.core.utils.listener.ReportListener;
 import com.example.pages.*;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(ReportListener.class)
 public class TestMainWorkFlow1 extends BaseTest {
     
     @Test(testName = "Demo Navigate To The Page")
