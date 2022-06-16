8c435b55aba64a85ccda55ef7760c5b95e2af4a8
Herry Nhat <herrynhat@gmail.com>
2022-06-16 16:48:21 +0700

    Team 1 NhatNM19 update close Ads
    Create function close Ads

--- demo-automation-java/src/test/java/com/example/pages/car_reviews_pages/CarReviewsNewCarPriceQuotePage.java
+++ demo-automation-java/src/test/java/com/example/pages/car_reviews_pages/CarReviewsNewCarPriceQuotePage.java
@@ -1,6 +1,7 @@
 package com.example.pages.car_reviews_pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.example.core.keyword.WebKeyword.chooseTypeOfSelect;
 import com.example.pages.BasePage;
@@ -23,10 +24,21 @@ public class CarReviewsNewCarPriceQuotePage extends BasePage {
      * @param make
      * @param model
      */
+
+    public void actionExitAds() {
+        try {
+            WebElement optDiveType = driver.findElement(By.xpath("//button[contains(@aria-label,'Close')]"));
+            keyword.click(optDiveType);
+        } catch (Exception e){
+
+        }
+        
+    }
     public void inputDataCar(String year, String make, String model){
         keyword.setValueForElement(keyword.findElement(selectYear), chooseTypeOfSelect.selectByValue, year);
         keyword.setValueForElement(keyword.findElement(selectMake), chooseTypeOfSelect.selectByValue, make);
         keyword.setValueForElement(keyword.findElement(selectModel), chooseTypeOfSelect.selectByValue, model);
+        actionExitAds();
         keyword.click(keyword.findElement(btnNext));
     }
 }
