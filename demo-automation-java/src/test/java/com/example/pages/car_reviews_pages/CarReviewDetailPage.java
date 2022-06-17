edb001dea4694e968ead81de11fbe5238134481c
DungNT146 <tandung1710@gmail.com>
2022-06-17 16:36:49 +0700

    team1-dungnt146-re-commit
    re-commit

--- demo-automation-java/src/test/java/com/example/pages/car_reviews_pages/CarReviewDetailPage.java
+++ demo-automation-java/src/test/java/com/example/pages/car_reviews_pages/CarReviewDetailPage.java
@@ -1,5 +1,7 @@
 package com.example.pages.car_reviews_pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,7 +23,23 @@ public class CarReviewDetailPage extends BasePage {
 			.findElement(By.xpath("//*[contains(@class,'ContentWrapper')]//*[text()='Hyundai Veloster']"));
 	private WebElement btnSeeDetail = keyword.findElement(By.id("columnButton1"));
 	private WebElement lnkTextCompareCar = keyword.findElement(By.xpath("//*[text()='Compare More']//span"));
+	private WebElement img10BestCarDeals = keyword.findElement(By.cssSelector("[alt='10 Best Car Deals']"));
+	private WebElement txtTitle10BestCarDeals = keyword.findElement(By.xpath("//*[text()='10 Best Car Deals']"));
+	private WebElement btnShopNow = keyword.findElement(By.xpath("//*[text()='Shop Now']"));
+	private WebElement txtTitleOfPhotoSection = keyword
+			.findElement(By.xpath("//h3[contains(text(),'Photos & Videos')]"));
+	private WebElement imgPhotoSection = keyword
+			.findElement(By.cssSelector("[class*='ScrollContent'] [class*='GridContainer']"));
+	private List<WebElement> listOfPhotoSection = imgPhotoSection.findElements(By.cssSelector("[class='css-bjn8wh']"));
+	private WebElement txtTitleAlsoFrom = keyword.findElement(By.xpath("//h4[contains(text(),'Also from')]"));
+	private WebElement listVehicleOfAlsoFrom = keyword
+			.findElement(By.xpath("//div[h4[contains(text(),'Also from')]]/following-sibling::*/ul"));
+	private List<WebElement> itemInListAlsoFrom = listVehicleOfAlsoFrom
+			.findElements(By.xpath("//div[h4[contains(text(),'Also from')]]/following-sibling::*/ul/li"));
 
+	/*
+	 * Compare Section
+	 */
 	public boolean verifyTitleCompareSection() {
 		keyword.scrollToElement(h4CompareSection);
 		return h4CompareSection.getText().equals(expectedTitle);
@@ -57,6 +75,60 @@ public class CarReviewDetailPage extends BasePage {
 		return false;
 	}
 
+	/*
+	 * 10 Best Car Deals Section
+	 */
+	public boolean verifyImgIsDisplayed() {
+		return img10BestCarDeals.isDisplayed();
+	}
+
+	public boolean verifyTitle10BestDealCar() {
+		return txtTitle10BestCarDeals.isDisplayed();
+	}
+
+	public boolean verifyImgAndTitle() {
+		if (verifyImgIsDisplayed()) {
+			if (verifyTitle10BestDealCar()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * Photo Section
+	 */
+	public boolean verifyTitlePhotoSection() {
+		return txtTitleOfPhotoSection.isDisplayed();
+	}
+
+	public void getText() {
+		System.out.println(keyword.findElement(By.cssSelector("[class*='IndicatorWrapper']")).getText().trim());
+	}
+
+	public boolean verifyNumberOfPhotoSection() {
+		System.out.println(listOfPhotoSection.size());
+		if (listOfPhotoSection.size() == 4) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Also From Section
+	 */
+
+	public boolean verifyTitleAlsoFromSection() {
+		return txtTitleAlsoFrom.isDisplayed();
+	}
+
+	public void verifyNumberOfAlsoFromSection() {
+		System.out.println(itemInListAlsoFrom.size());
+	}
+
+	/*
+	 * Action on page
+	 */
 	public void clickOnSeeDetailButton() {
 		keyword.clickByJS(btnSeeDetail);
 	}
@@ -65,8 +137,26 @@ public class CarReviewDetailPage extends BasePage {
 		keyword.clickByJS(lnkTextCompareCar);
 	}
 
+	public void clickOnShopNowButton() {
+		keyword.clickByJS(btnShopNow);
+	}
+
 	public String getTitleAndNameOfSecondCar() {
 		return txtTitleOfSecondCar + " " + txtNameOfSecondCar;
+	}
+
+	public void clickOnFirstItemInListAlsoFrom() {
+		keyword.clickByJS(itemInListAlsoFrom.get(0));
+	}
+	/*
+	 * parse string to int
+	 * @param input string
+	 */
+	public int parseStringToNumber(String input)
+	{
+	String subString= input.substring(input.lastIndexOf("f") + 2);
+	int number=Integer.parseInt(subString);
+	return number;
 	}
 
 	public static String expectedTitle = "Compare to Similar Vehicles";
---
+++ demo-automation-java/src/test/java/com/example/tests/car_reviews_tests/CarReviewModuleAlsoFromTest.java
@@ -0,0 +1,29 @@
+package com.example.tests.car_reviews_tests;
+
+import org.testng.annotations.Test;
+
+import com.example.pages.BasePage;
+import com.example.pages.car_reviews_pages.CarReviewDetailPage;
+import com.example.pages.car_reviews_pages.CarReviewPage;
+import com.example.tests.BaseTest;
+
+public class CarReviewModuleAlsoFromTest extends BaseTest{
+    @Test
+    public void verifyThatTheUIOf10BestCarDealsSectiondisplay() {
+        BasePage basePage = new BasePage(driver);
+        basePage.navigateToPage("Reviews");
+        CarReviewPage carReviewPage = new CarReviewPage(driver);
+        carReviewPage.fillInformation();
+        CarReviewDetailPage carReviewDetailPage = new CarReviewDetailPage(driver);
+        carReviewDetailPage.clickOnFirstItemInListAlsoFrom();
+    }
+    @Test
+    public void verifyThatTheBrowserCouldBeRedirectedToCarReviewPageOfItemInTheAlsoFromListAfterClickOnIt() {
+        BasePage basePage = new BasePage(driver);
+        basePage.navigateToPage("Reviews");
+        CarReviewPage carReviewPage = new CarReviewPage(driver);
+        carReviewPage.fillInformation();
+        CarReviewDetailPage carReviewDetailPage = new CarReviewDetailPage(driver);
+        carReviewDetailPage.clickOnFirstItemInListAlsoFrom();
+    }
+}
